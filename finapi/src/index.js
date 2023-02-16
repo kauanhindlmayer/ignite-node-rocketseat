5e080a4e298e1bce8bb2cebcfb3e2f3c548aa28e
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-02-15 21:15:42 -0300

    validando cpf existente

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -1,5 +1,38 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+
+app.post("/account", (request, response) => {
+  const { name, cpf } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }
+
+  customers.push({
+    id: uuidv4(),
+    name,
+    cpf,
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
 
 app.listen(3333);
