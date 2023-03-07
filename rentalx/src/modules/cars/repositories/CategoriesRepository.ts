b0cb4bca14c7117bb0366168d6954b8a7cd36c25
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-03-06 21:44:10 -0300

    conhecendo o singleton pattern

--- rentalx/src/modules/cars/repositories/CategoriesRepository.ts
+++ rentalx/src/modules/cars/repositories/CategoriesRepository.ts
@@ -7,8 +7,16 @@ import {
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
-  constructor() {
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
     this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
   }
 
   create({ name, description }: ICreateCategoryDTO): void {
