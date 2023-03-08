9cc8190760099a3caef1b94ebb6ecd3065eb6c06
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-03-07 21:02:52 -0300

    criando use case de especificacao

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { createCategoryController } from "../modules/cars/useCases/CreateCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategory";
 
 const categoriesRoutes = Router();
