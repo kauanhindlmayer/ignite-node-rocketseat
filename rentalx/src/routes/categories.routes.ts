f2c0e97d248bce0d2aa0a33ff8f6b2b0130d54d0
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-03-07 21:34:01 -0300

    criando o upload de arquivos

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/CreateCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategory";
 
 const categoriesRoutes = Router();
+
+const uploads = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
@@ -12,5 +17,17 @@ categoriesRoutes.post("/", (request, response) => {
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
 });
+
+categoriesRoutes.post(
+  "/import",
+  uploads.single("file"),
+  (request, response) => {
+    const { file } = request;
+
+    console.log(file);
+
+    return response.send();
+  }
+);
 
 export { categoriesRoutes };
