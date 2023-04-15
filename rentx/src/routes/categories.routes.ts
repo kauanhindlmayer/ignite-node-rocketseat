6a5b1e62ce1e98f52ff18928a1a1583648d96fef
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-04-15 17:44:48 -0300

    refatorando category useCase

--- rentx/src/routes/categories.routes.ts
+++ rentx/src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import multer from "multer";
 
-import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import createCategoryController from "../modules/cars/useCases/createCategory";
 import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategory";
 
@@ -12,8 +12,7 @@ const uploads = multer({
 });
 
 categoriesRoutes.post("/", (request, response) => {
-  console.log("Reload working!");
-  return createCategoryController.handle(request, response);
+  return createCategoryController().handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
