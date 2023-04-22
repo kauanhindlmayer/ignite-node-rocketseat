73af3deeff0a08f6291639e166ffd0402437a857
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-04-22 10:04:41 -0300

    criando repositório de usuário

--- rentx/src/shared/container/index.ts
+++ rentx/src/shared/container/index.ts
@@ -2,8 +2,12 @@ import { container } from "tsyringe";
 
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
+
 import { SpecificationRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
 import { ISpecificationRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
@@ -13,4 +17,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationRepository>(
   "SpecificationRepository",
   SpecificationRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
 );
