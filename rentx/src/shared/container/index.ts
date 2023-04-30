0103644648a5e2fa94f25f3bf8817c4672c0bc6e
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-04-30 11:34:44 -0300

    imports da aplicação

--- rentx/src/shared/container/index.ts
+++ rentx/src/shared/container/index.ts
@@ -1,13 +1,13 @@
 import { container } from "tsyringe";
 
-import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
-import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
+import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
+import { CategoriesRepository } from "@modules/cars/repositories/implementations/CategoriesRepository";
 
-import { SpecificationRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
-import { ISpecificationRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+import { SpecificationRepository } from "@modules/cars/repositories/implementations/SpecificationsRepository";
+import { ISpecificationRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 
-import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
-import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+import { UsersRepository } from "@modules/accounts/repositories/implementations/UsersRepository";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
