19b9f539e9534019b46d3a37a735aceebfcdecba
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-05-01 19:25:28 -0300

    continuando o caso de uso de carros

--- rentx/src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ rentx/src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from "@shared/errors/AppError";
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
 import { CreateCarUseCase } from "../createCar/CreateCarUseCase";
 
@@ -11,14 +12,54 @@ describe("Create Car", () => {
   });
 
   it("should be able to create a new car", async () => {
-    await createCarUseCase.execute({
-      name: "Name",
-      description: "Description",
+    const car = await createCarUseCase.execute({
+      name: "name",
+      description: "description",
       daily_rate: 100,
       license_plate: "license_plate",
       fine_amount: 60,
-      brand: "Brand",
-      category_id: "category",
+      brand: "brand",
+      category_id: "category_id",
     });
+
+    expect(car).toHaveProperty("id");
+  });
+
+  it("should not be able to create a car with existing license_plate", async () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: "first Car",
+        description: "description",
+        daily_rate: 100,
+        license_plate: "license_plate",
+        fine_amount: 60,
+        brand: "brand",
+        category_id: "category_id",
+      });
+
+      await createCarUseCase.execute({
+        name: "second Car",
+        description: "description",
+        daily_rate: 100,
+        license_plate: "license_plate",
+        fine_amount: 60,
+        brand: "brand",
+        category_id: "category_id",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should be able to create a car with available true by default", async () => {
+    const car = await createCarUseCase.execute({
+      name: "name",
+      description: "description",
+      daily_rate: 100,
+      license_plate: "license_plate",
+      fine_amount: 60,
+      brand: "brand",
+      category_id: "category_id",
+    });
+
+    expect(car.available).toBe(true);
   });
 });
