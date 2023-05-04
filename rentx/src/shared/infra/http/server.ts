3e7dc43a879668c0691c6d782f2a2b0c9a558beb
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-05-03 21:23:47 -0300

    criando o seed de usuário

--- rentx/src/shared/infra/http/server.ts
+++ rentx/src/shared/infra/http/server.ts
@@ -6,10 +6,11 @@ import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
 import swaggerFile from "../../../swagger.json";
 
-import "@shared/infra/typeorm";
-
 import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
+
+import createConnection from "@shared/infra/typeorm";
+createConnection();
 
 const app = express();
 
